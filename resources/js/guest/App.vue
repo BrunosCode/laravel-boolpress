e7a8943c1cbc250e0c7860c762051c8532c1ab5e
brunEdo <edobruno.m@gmail.com>
2021-11-22 17:39:11 +0100

    vue welcome page

---
+++ resources/js/guest/App.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+    <Header/>
+        <div class="flex-center position-ref full-height">
+                    
+            <div class="content">
+                    <div class="title m-b-md">
+                        Laravel
+                    </div>
+
+                    <div class="links">
+                        <a href="">Blog</a>
+                    </div>
+            </div>
+        </div>
+</div>
+
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+    name: "App",
+    components: {
+        Header
+    },
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
+
+<style>
+    html, body {
+        background-color: #fff;
+        color: #636b6f;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+    }
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
